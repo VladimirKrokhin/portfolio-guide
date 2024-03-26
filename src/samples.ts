9b6c97978557a692cc20c9bb9087658a1f4ac13a
Vladimir Krokhin <119413075+VladimirKrokhin@users.noreply.github.com>
2024-03-26 20:10:51 +0500

    Update samples.ts

--- src/samples.ts
+++ src/samples.ts
@@ -2,36 +2,13 @@ export const SAMPLES = {
     "Creating-ACE-Editor": [
         'День Открытых Дверей',
         'ПрофПробы',
-        'Shadow-DOM',
-        'Basic-options',
-        'Hard-wrapping'
     ],
     "Interacting-with-editor": [
-        'Autoresize',
-        'Keyboard-shortcuts',
-        'Adding-new-command',
-        'Scrollable-page',
-        'Settings-menu',
-        'Static-Code-Highlighter',
-        'StatusBar',
-        'ToolBar',
-        'Transform',
-        'Reveal-range',
-        'Providing-custom-annotations',
-        'Listening-to-events',
-        'Scrollbars',
-        'Creating-own-validation',
-        'Customizing-line-numbers'
     ],
     "Customising-the-appearance": [
-        'Creating-new-theme'
     ],
     "Extensions": [
-        'Code-Lens',
-        'Custom-completions'
     ],
     "Language-features": [
-        'Validation-against-JSON-schema',
-        'Typescript-language-service'
     ]
 };
