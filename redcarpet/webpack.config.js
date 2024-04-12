5ac8659351af1f3559b7e8c6ecb2ba6f9dcae160
Vladimir <krokhinvladimir2004@gmail.com>
2024-04-12 10:24:02 +0500

    Still trying to deploy no. 33

--- redcarpet/webpack.config.js
+++ redcarpet/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: "ui/index.js",
+    entry: "./ui/index.js",
     mode: "production",
     module: {
         rules: [
